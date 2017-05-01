fa618e1b77973e9561a9fcf10e03d103463d100f
logicp <emmanuel@openconcept.ca>
2017-04-30 22:38:33 -0400

    AccountActivity: Added structure to handle login for two potential AsyncTasks

--- app/src/main/java/stronglogic/ruviuz/AccountActivity.java
+++ app/src/main/java/stronglogic/ruviuz/AccountActivity.java
@@ -3,34 +3,36 @@ package stronglogic.ruviuz;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.app.LoaderManager.LoaderCallbacks;
-
 import android.content.CursorLoader;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
-
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import stronglogic.ruviuz.tasks.LoginTask;
+import stronglogic.ruviuz.tasks.RegistrationTask;
 
 import static android.Manifest.permission.READ_CONTACTS;
 
@@ -38,6 +40,8 @@ import static android.Manifest.permission.READ_CONTACTS;
  * A login screen that offers login via email/password.
  */
 public class AccountActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+
+    public final String TAG = this.getClass().getSimpleName();
 
     /**
      * Id to identity READ_CONTACTS permission request.
@@ -54,19 +58,47 @@ public class AccountActivity extends AppCompatActivity implements LoaderCallback
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
-    private UserLoginTask mAuthTask = null;
+    private LoginTask mAuthTask = null;
 
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+    private RadioGroup registrationTabs;
+
+    private boolean registrationMode = true;
+
+    private LoginTask loginTask;
+    private RegistrationTask registrationTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
         // Set up the login form.
+
+        registrationTabs = (RadioGroup) findViewById(R.id.numpickGrp);
+        registrationTabs.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+                switch (checkedId) {
+
+                    case R.id.registrationModeBtn:
+                        AccountActivity.this.registrationMode = true;
+                        break;
+
+                    case R.id.loginModeBtn:
+                        AccountActivity.this.registrationMode = false;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        });
+
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
 
@@ -74,11 +106,8 @@ public class AccountActivity extends AppCompatActivity implements LoaderCallback
         mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
-                }
-                return false;
+                Log.d(TAG, textView.getText().toString());
+                return true;
             }
         });
 
@@ -86,7 +115,12 @@ public class AccountActivity extends AppCompatActivity implements LoaderCallback
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                attemptLogin();
+
+                if (AccountActivity.this.registrationMode == true) {
+                    attemptRegistration();
+                } else {
+                    attemptLogin();
+                }
             }
         });
 
@@ -137,17 +171,32 @@ public class AccountActivity extends AppCompatActivity implements LoaderCallback
         }
     }
 
+    private void attemptRegistration() {
 
+        if (registrationTask == null) {
+            registrationTask = new RegistrationTask(mEmailView.getText().toString(), mPasswordView.getText().toString(), MainActivity.baseUrl, new RegistrationTask.AsyncResponse() {
+                @Override
+                public void processFinish(String output) {
+                    Log.d(TAG, "Registration result: " + output);
+                }
+            });
+        }
+    }
     /**
      * Attempts to sign in or register the account specified by the login form.
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
     private void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
 
+        if (loginTask == null) {
+            loginTask = new LoginTask(mEmailView.getText().toString(), mPasswordView.getText().toString(), MainActivity.baseUrl, new LoginTask.AsyncResponse() {
+                @Override
+                public void processFinish(String output) {
+                    Log.d(TAG, "Login result:" + output);
+                }
+            });
+        }
         // Reset errors.
         mEmailView.setError(null);
         mPasswordView.setError(null);
@@ -185,8 +234,8 @@ public class AccountActivity extends AppCompatActivity implements LoaderCallback
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(email, password);
-            mAuthTask.execute((Void) null);
+//            mAuthTask = new UserLoginTask(email, password);
+//            mAuthTask.execute((Void) null);
         }
     }
 
---
+++ app/src/main/java/stronglogic/ruviuz/tasks/RegistrationTask.java
@@ -0,0 +1,134 @@
+package stronglogic.ruviuz.tasks;
+
+import android.os.AsyncTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by logicp on 11/27/16.
+ * Logs into Ruviuz
+ */
+public class RegistrationTask extends AsyncTask<String[], String, boolean[]> {
+
+    private static final String TAG = "RuviuzREGISTRATIONTASK";
+
+    private String email, password, authToken, baseUrl;
+
+    private AsyncResponse delegate;
+
+    public RegistrationTask(String email, String password, String baseUrl, AsyncResponse delegate) {
+        this.email = email;
+        this.password = password;
+        this.baseUrl = baseUrl;
+        this.delegate = delegate;
+    }
+
+    public interface AsyncResponse {
+        void processFinish(String output);
+    }
+
+    @Override
+    protected boolean[] doInBackground(String[]... params) {
+        boolean mBool[] = new boolean[1];
+        mBool[0] = loginRuviuz(email, password);
+
+        return mBool;
+    }
+
+    @Override
+    protected void onPostExecute(boolean[] result) {
+        if (!result[0]) {
+            delegate.processFinish("{\"error\":\"login failed\"}");
+        } else {
+            super.onPostExecute(result);
+            String returnString = "{\"token\":\"" + this.authToken + "\"}";
+            delegate.processFinish(returnString);
+        }
+    }
+
+    //curl -i -X POSontent-Type: application/json" -d '{"email":"jiggamortis","password":"calcutta"}' http://127.0.0.1:5000/login
+
+
+    private boolean loginRuviuz(String email, String password) {
+        String loginUrl = baseUrl + "/login";
+        HttpURLConnection connection = null;
+        BufferedReader reader = null;
+
+        try {
+            URL url = new URL(loginUrl);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("Accept", "application/json");
+            JSONObject jsonLogin = new JSONObject();
+
+            try {
+                jsonLogin.put("email", email);
+                jsonLogin.put("password", password);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            String loginCredsJson = jsonLogin.toString();
+
+            connection.connect();
+
+            Writer writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), "UTF-8"));
+
+            writer.write(loginCredsJson);
+            writer.close();
+
+            InputStream stream = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(stream));
+            StringBuffer buffer = new StringBuffer();
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                buffer.append(line);
+            }
+
+            try {
+                JSONObject respJson = new JSONObject(buffer.toString());
+                if (respJson.has("authToken")) {
+                    this.authToken = respJson.getString("authToken");
+                    return true;
+                } else {
+                    return false;
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return false;
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
